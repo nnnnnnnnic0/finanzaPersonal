404fd45e6e4c54e7a00c0c14a440fdebb74d8602
nnnnnnnnic0 <ser.reyesv@gmail.com>
2025-05-09 16:25:28 -0400

    collectedstatic y cambion en settings.py

--- ejercicio1/settings.py
+++ ejercicio1/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w2t!a4t66f=8(tp^by0gorh%=gfzzv3oo7ftqog@j!j3b%=4dy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['IP_PUBLICA_DE_TU_INSTANCIA', 'localhost']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Application definition
